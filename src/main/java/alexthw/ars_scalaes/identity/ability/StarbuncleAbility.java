8251feb606a8ebd02dd6bb856681d6c4644def06
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2022-07-29 21:40:10 +0200

    vers. bump

--- src/main/java/alexthw/ars_scalaes/identity/ability/StarbuncleAbility.java
+++ src/main/java/alexthw/ars_scalaes/identity/ability/StarbuncleAbility.java
@@ -1,22 +1,18 @@
 package alexthw.ars_scalaes.identity.ability;
 
-import com.hollingsworth.arsnouveau.api.scrying.CompoundScryer;
-import com.hollingsworth.arsnouveau.api.scrying.TagScryer;
 import com.hollingsworth.arsnouveau.common.entity.Starbuncle;
-import com.hollingsworth.arsnouveau.common.ritual.RitualScrying;
 import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import draylar.identity.ability.IdentityAbility;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.tags.BlockTags;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.common.Tags;
 
 public class StarbuncleAbility<S extends Starbuncle> extends IdentityAbility<S> {
     @Override
     public void onUse(Player player, S s, Level level) {
-        RitualScrying.grantScrying((ServerPlayer) player, 3 * 20 * 60, new CompoundScryer(new TagScryer(Tags.Blocks.ORES_GOLD), new TagScryer(BlockTags.GOLD_ORES)));
+        player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3000, 1 ,false,false,false));
     }
 
     @Override
